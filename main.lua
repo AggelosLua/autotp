1375af816a450e8da3f81fc73098c041b45eae97
Aggelos <87412823+AggelosLua@users.noreply.github.com>
2022-12-31 12:08:00 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,14 @@
+local succ,err = pcall(function()
+    kometaLoader = loadstring(game:HttpGet("https://s.kometa.ga/kometa.lua"))();
+    NotificationLibLoader = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
+    MainDependency = loadstring(game:HttpGet("https://github.com/AggelosLua/autotp/raw/main/main.lua"))()
+end);
+if err then
+    error("Failed to load a dependecy.");
+end;
+NotificationLibLoader.new("success", "Success", "Successfully rejoined and ran dependencies.") 
+wait(1)
+NotificationLibLoader.new("warning", "Warnings", "Rejoined after kick.")
 _ = game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(Child)
     if Child.Name == 'ErrorPrompt' and Child:FindFirstChild('MessageArea') and Child.MessageArea:FindFirstChild("ErrorFrame") then
         wait(1);
@@ -10,15 +21,5 @@ _ = game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(
         local kometaLoader;
         local NotificationLibLoader;
         local queue_on_teleport = syn and syn.queue_on_teleport or queue_on_teleport;
-        local succ,err = pcall(function()
-            kometaLoader = loadstring(game:HttpGet('https://s.kometa.ga/kometa.lua'))();
-            NotificationLibLoader = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
-        end);
-        if err then
-            error("Failed to load a dependecy.");
-        end;
-        NotificationLibLoader.new("success", "Success", "Successfully rejoined and ran dependencies.") 
-        wait(1)
-        NotificationLibLoader.new("warning", "Warnings", "Rejoined after kick.")
     end
 end)
